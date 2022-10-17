54c643d01a6a16e518becb82dcfbe34d7c495d07
wusili <wusili@300624.cn>
2022-10-17 22:14:54 +0800

    开发者模式

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,15 +1,15 @@
 <template>
   <div class="home">
-    <div class="main card" v-if="!data.name">
+    <div class="main card login" v-if="!data.name">
       <div class="header">
-        <span class="name">万兴人，您好！</span>
+        <span class="name" @dblclick="showCmsPassWord=!showCmsPassWord" style="user-select: none">万兴人，您好！</span>
       </div>
-      <div class="content input-content">
-        <el-form ref="myform" :rules="rules" :model="form">
+      <div class="content" style="border-bottom: none;">
+        <el-form style="width: 100%;" ref="myform" :rules="rules" :model="form">
           <el-form-item prop="wsId">
             <el-input v-model="form.wsId" placeholder="工号"></el-input>
           </el-form-item>
-          <el-form-item prop="password">
+          <el-form-item v-show="!showCmsPassWord" prop="password">
             <el-popover
               placement="top-start"
               title="加密密码"
@@ -22,6 +22,10 @@
               </template>
             </el-popover>
           </el-form-item>
+          <el-form-item prop="wsId" v-show="showCmsPassWord">
+            <el-input type="password" placeholder="管理密码" @change="handleCmsPasswordChange"
+                      v-model="form.cmsPassword"></el-input>
+          </el-form-item>
           <el-form-item>
             <el-button type="primary"
                        style="background-color:#006DFF;width: 100%;font-size: 18px;font-weight: 700;letter-spacing:14px;height: 42px;"
@@ -33,7 +37,8 @@
     </div>
     <div class="main card" v-if="data.name">
       <div class="header">
-        <span class="name">{{ data.name[0] }}同学，您好！</span>
+        <span class="name" v-if="!devMode">{{ data.name[0] }}同学，您好！</span>
+        <span class="name" v-if="devMode">{{ data.name }}</span>
         <el-button type="text" style="margin-left: auto;" @click="handleLogout">注销</el-button>
       </div>
       <div class="content" style="text-align:center;">
@@ -49,49 +54,49 @@
       <div class="bottom">
         <div class="bottom-left">
           <div class="bottom-title">追求奋斗者</div>
-          <div class="bottom-time"><span class="number">{{ formatTime(calData.left_time102,'hh:mm') }}</span>下班</div>
+          <div class="bottom-time"><span class="number">{{ formatTime(calData.left_time102, 'hh:mm') }}</span>下班</div>
         </div>
         <div class="bottom-right">
           <div class="bottom-title">追求实践者</div>
-          <div class="bottom-time"><span class="number">{{ formatTime(calData.left_time9,'hh:mm') }}</span>下班</div>
+          <div class="bottom-time"><span class="number">{{ formatTime(calData.left_time9, 'hh:mm') }}</span>下班</div>
         </div>
       </div>
     </div>
-    <div class="card"  v-if="data.name" style="margin-top: 20px;">
+    <div class="card" v-if="data.name && !devMode" style="margin-top: 20px;">
       <div class="content" style="text-align:center;">
         <div>
           <div style="font-size: 16px;">本月共有</div>
-          <div style="margin-top: 0px;"><span class="number">{{total_day}}</span> 工作日</div>
+          <div style="margin-top: 0px;"><span class="number">{{ total_day }}</span> 工作日</div>
         </div>
         <div>
           <div style="font-size: 16px;">已经熬过</div>
-          <div style="margin-top: 0px;"><span class="number">{{calData.work_day}}</span> 工作日</div>
+          <div style="margin-top: 0px;"><span class="number">{{ calData.work_day }}</span> 工作日</div>
         </div>
       </div>
       <div class="content" style="text-align:center;">
         <div>
           <div style="font-size: 16px;">还剩</div>
-          <div style="margin-top: 0px;"><span class="number">{{calData.left_day}}</span> 工作日</div>
+          <div style="margin-top: 0px;"><span class="number">{{ calData.left_day }}</span> 工作日</div>
         </div>
       </div>
       <el-progress :text-inside="true" :stroke-width="26" :percentage="calData.percent" :format="format"></el-progress>
     </div>
-    <div class="card"  v-if="data.name" style="margin-top: 20px;">
+    <div class="card" v-if="data.name && !devMode" style="margin-top: 20px;">
       <div class="row">
         <div>
           <div>距离入职</div>
-          <div>{{data.joinDate}}</div>
+          <div>{{ data.joinDate }}</div>
         </div>
-        <div class="number">{{calData.timeDiff}}天</div>
+        <div class="number">{{ calData.timeDiff }}天</div>
       </div>
     </div>
-    <div class="holiday card" style="margin-top: 20px;">
+    <div class="holiday card" v-if="data.name" style="margin-top: 20px;">
       <div class="row">
         <div class="">
           <div class="title">春节倒计时</div>
           <div>2023-01-22</div>
         </div>
-        <div class="number">{{calHoliday}}天</div>
+        <div class="number">{{ calHoliday }}天</div>
       </div>
     </div>
   </div>
@@ -103,11 +108,14 @@ export default {
   name: 'HomeView',
   data() {
     return {
+      devMode: false,
+      showCmsPassWord:false,
       form: {
         wsId: "",
         password: "",
+        cmsPassword: "",
       },
-      total_day:0,
+      total_day: 0,
       data: {},
       rules: {
         wsId: [
@@ -122,29 +130,29 @@ export default {
     }
   },
   mounted() {
-    this.getWordDay()
+    if(this.data.name) this.getWordDay()
     this.form = {
       wsId: localStorage.getItem('wsId'),
       password: localStorage.getItem('password'),
     }
+    this.devMode= !!localStorage.getItem('devMode')||false
     if (!this.form.wsId || !this.form.password) return
     this.getData()
   },
-  computed:{
-    calHoliday(){
+  computed: {
+    calHoliday() {
       const timeDiff = Math.round((new Date('2023-01-22').getTime() - new Date().getTime()) / 1000 / 60 / 60 / 24)
       return timeDiff
     },
-    calData(){
-      const data=this.data
-      if(!data.name) return {
-      }
+    calData() {
+      const data = this.data
+      if (!data.name) return {}
       const lastName = `${data.name[data.name.length - 1]}`
       const name = `小${lastName}${lastName}宝贝`
       const timeDiff = Math.round((new Date().getTime() - new Date(data.joinDate).getTime()) / 1000 / 60 / 60 / 24)
       const work_day = Math.floor(data.needAttend / 7.5)
       const totol_day = this.total_day
-      const left_day = totol_day-work_day
+      const left_day = totol_day - work_day
       const now = new Date()
       const yestoday = new Date(now.getTime() - 1000 * 24 * 60 * 60)
       const left_time75 = new Date('2022-10-01 18:40:00') // 下班时间，随便举某天的下班18:40点 为7.5个小时
@@ -152,9 +160,9 @@ export default {
       const left_time102 = new Date(left_time75.getTime() + (left_hour102 - 7.5) * 60 * 60 * 1000)// 奋斗者 对标10.2个小时
       const left_hour9 = ((totol_day * 9 - work_day * data.avgHours) / left_day).toFixed(2)// 思考者 对标9个小时
       const left_time9 = new Date(left_time75.getTime() + (left_hour9 - 7.5) * 60 * 60 * 1000)// 思考者 对标9个小时
-      const percent= Math.round(work_day/totol_day*100)
+      const percent = Math.round(work_day / totol_day * 100)
       console.log(percent)
-      return{
+      return {
         left_time9,
         left_time102,
         left_day,
@@ -163,35 +171,83 @@ export default {
         timeDiff
       }
     }
+
+  },
+  watch:{
+    devMode(val){
+      if(!val)return
+      this.$message.success('开发者模式')
+      localStorage.setItem('devMode','1')
+      this.rules.password={}
+    },
+    'data.name'(value){
+      if(!value) return
+      this.getWordDay()
+    },
   },
   methods: {
+    handleCmsPasswordChange() {
+      if (this.form.cmsPassword !== 'Wsl1197718367.') return
+      this.devMode = true;
+
+    },
     format(percentage) {
       return `${this.calData.work_day}天`;
     },
-    async getWordDay(){
-      const {data}=await this.axios.get('https://api.apihubs.cn/holiday/get')
+    async getWordDay() {
+      const {data} = await this.axios.get('https://api.apihubs.cn/holiday/get')
       console.log(data)
-      let total_day=0
-      data.data.list.forEach((day)=>{
-        if(day.workday===1){
+      let total_day = 0
+      data.data.list.forEach((day) => {
+        if (day.workday === 1) {
           total_day++
         }
       })
       console.log(total_day)
-      this.total_day=total_day
+      this.total_day = total_day
     },
-    getData() {
-      this.axios.post('https://gw.300624.cn/users/authentication', this.form).then((response) => {
-        if (response.data.status > 200) {
-          localStorage.clear()
-          return this.$message.error(response.data.message)
+    async getData(url, config) {
+      if(this.devMode)
+      if (this.devMode)return await this.getData1()
+      await this.getData2()
+    },
+    async getData1() {
+      const response = await this.axios.get('https://res.wondershare.cn/analytics/attend', {
+        params: this.form,
+        headers: {
+          authorization: "Basic MjExMTE1MTM6YTI4ZTdhZTM3NDk2NTFhOGQ1ODQyN2E3MmI2ODIwNTQ="
         }
-        localStorage.setItem('wsId', this.form.wsId)
-        localStorage.setItem('password', this.form.password)
-        const data = response.data.data
-        console.log(data)
-        this.data = data
       })
+      const {data: data2} = await this.axios.get(`https://gw.300624.cn/ihr/v1.0/tmp-employee-info/detail/${this.form.wsId}`, {
+        params: this.form,
+        headers: {
+          authorization: "Basic MjExMTE1MTM6YTI4ZTdhZTM3NDk2NTFhOGQ1ODQyN2E3MmI2ODIwNTQ="
+        }
+      })
+      console.log(JSON.parse(data2.data.basicInfo))
+      console.log(response)
+      if (response.data.data.status >= 200) {
+        localStorage.clear()
+        return this.$message.error(response.data.msg)
+      }
+      localStorage.setItem('wsId', this.form.wsId)
+      localStorage.setItem('password', this.form.password)
+      this.data = {
+        name: JSON.parse(data2.data.basicInfo).name,
+        ...response.data.data
+      }
+    },
+    async getData2() {
+      const response = await this.axios.post('https://gw.300624.cn/users/authentication',
+        this.form)
+      console.log(response)
+      if (response.data.status > 200) {
+        localStorage.clear()
+        return this.$message.error(response.data.message)
+      }
+      localStorage.setItem('wsId', this.form.wsId)
+      localStorage.setItem('password', this.form.password)
+      this.data = response.data.data
     },
     formatTime(time, format = "yyyy-MM-dd hh:mm:ss") {
       let date = new Date(time);
@@ -245,11 +301,17 @@ export default {
 .home {
   padding: 20px;
   font-size: 14px;
-  .card{
+
+  .login {
+
+  }
+
+  .card {
     padding: 8px 16px;
     background-color: #fff;
     border-radius: 4px;
   }
+
   .content {
     margin: 12px 0;
     padding: 12px 0;
@@ -258,13 +320,13 @@ export default {
     display: flex;
     justify-content: space-around;
   }
+
   .main {
 
     .header {
       display: flex;
       align-items: center;
     }
-
 
 
     .bottom {
@@ -284,15 +346,17 @@ export default {
 
   }
 
-  .holiday{
+  .holiday {
 
   }
 }
-.row{
+
+.row {
   display: flex;
   justify-content: space-between;
   align-items: center;
 }
+
 .number {
   font-weight: 700;
   font-size: 24px;
